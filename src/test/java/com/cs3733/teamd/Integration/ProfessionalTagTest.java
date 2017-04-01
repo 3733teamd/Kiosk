3d44b8c0df9a41b7e99c1921b47a03cafe545572
Stephen Michelini <sdmichelini@wpi.edu>
2017-04-01 12:50:24 -0400

    Added Integration Tests

---
+++ src/test/java/com/cs3733/teamd/Integration/ProfessionalTagTest.java
@@ -0,0 +1,15 @@
+package com.cs3733.teamd.Integration;
+
+/**
+ * Created by sdmichelini on 4/1/17.
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Itegration Tests for the Professional and the Tag objects
+ */
+public class ProfessionalTagTest {
+
+}
