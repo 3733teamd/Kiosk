8cb125a72947c9117b8e3f32f20fca24fa8c5530
DESKTOP-0JMVFJF\Me <aenemeth@wpi.edu>
2017-04-04 17:56:29 -0400

    Expanded dir

--- src/main/java/com/cs3733/teamd/Controller/Directory.java
+++ src/main/java/com/cs3733/teamd/Controller/Directory.java
@@ -7,10 +7,21 @@ import com.cs3733.teamd.Model.Tag;
 import java.util.ArrayList;
 
 /**
- * Created by Me on 4/4/2017.
+ * Used by CONTROLLERS to store ALL ENTITIES
+ * Knows the DB Connection so it can SAVE ENTITIES
  */
 public class Directory {
-    ArrayList<Node> allNodes = new ArrayList<Node>();
-    ArrayList<Tag> allTags = new ArrayList<Tag>();
-    ArrayList<Professional> allProfs = new ArrayList<Professional>();
+    private ArrayList<Node> allNodes = new ArrayList<Node>();
+    private ArrayList<Tag> allTags = new ArrayList<Tag>();
+    private ArrayList<Professional> allProfs = new ArrayList<Professional>();
+
+    public Directory(ArrayList<Node> nodes,ArrayList<Tag> tags, ArrayList<Professional> profs){
+        allNodes = nodes;
+        allTags = tags;
+        allProfs = profs;
+    }
+
+    public ArrayList<Tag> getAllTags() {
+        return allTags;
+    }
 }
