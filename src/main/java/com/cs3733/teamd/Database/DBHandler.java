d895bcaba14b79d7673b39c04f4ba699b9f27db1
DESKTOP-0JMVFJF\Me <aenemeth@wpi.edu>
2017-04-04 21:27:57 -0400

    editing save

--- src/main/java/com/cs3733/teamd/Database/DBHandler.java
+++ src/main/java/com/cs3733/teamd/Database/DBHandler.java
@@ -400,7 +400,7 @@ public class DBHandler {
         for(Node n : dir.getAllNodes()){
             for(Node edge : n.getNodes()){
                 //create adjacent node
-                s.execute("INSERT INTO AdjacentNode "+n.)
+                s.execute("INSERT INTO AdjacentNode ("+n.getID()+","+edge.getID()+")");
             }
             //fills in NODETAG
             for(Tag t : n.getTags()){
