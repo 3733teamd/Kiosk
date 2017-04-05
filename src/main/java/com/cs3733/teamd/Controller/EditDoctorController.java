135973084b7d3165729d6c9d8852d5fdca876ad7
DESKTOP-0JMVFJF\Me <aenemeth@wpi.edu>
2017-04-05 01:53:55 -0400

    new branch

--- src/main/java/com/cs3733/teamd/Controller/EditDoctorController.java
+++ src/main/java/com/cs3733/teamd/Controller/EditDoctorController.java
@@ -22,6 +22,7 @@ import java.util.LinkedList;
  */
 public class EditDoctorController extends  AbsController{
 
+    public Label submitFeedback;
     LinkedList<String> userOptionList = new LinkedList<String>();
 
     public Button largerTextButton;
