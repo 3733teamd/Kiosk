5aa96837078976329661326925648043e80b58af
wittenber0 <ryanwwittenberg@gmail.com>
2017-04-06 14:22:40 -0400

    Create Controller

--- src/main/java/com/cs3733/teamd/Controller/EditTagController.java
+++ src/main/java/com/cs3733/teamd/Controller/EditTagController.java
@@ -1,7 +1,24 @@
 package com.cs3733.teamd.Controller;
 
+import com.cs3733.teamd.Model.Directory;
+import com.cs3733.teamd.Model.Tag;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.util.ArrayList;
+
 /**
  * Created by Ryan on 4/6/2017.
  */
 public class EditTagController {
+    Directory dir = Directory.getInstance();
+    public TextArea tagTextArea;
+    public TextField searchTagBar;
+
+    private void intitalize(){
+        ArrayList<Tag> allTags = dir.getAllTags();
+        String fillBox;
+
+    }
+    
 }
