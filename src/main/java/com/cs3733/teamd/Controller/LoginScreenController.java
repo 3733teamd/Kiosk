6bf47a5779a5e6a03dda1259e638e01a64d6a214
BENSLAPTOP\benjamin <bdshaffer31@gmail.com>
2017-04-06 22:40:02 -0400

    big changes to edit tag screen

--- src/main/java/com/cs3733/teamd/Controller/LoginScreenController.java
+++ src/main/java/com/cs3733/teamd/Controller/LoginScreenController.java
@@ -1,7 +1,14 @@
 package com.cs3733.teamd.Controller;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 /**
  * Created by Ryan on 4/6/2017.
  */
 public class LoginScreenController {
+    public TextField username;
+    public PasswordField password;
+    public Button submitButton;
 }
