4e4af508422226f5883708e8f2968f8b220543d0
Wheeleral <awheeler2@wpi.edu>
2017-04-07 08:52:45 -0400

    added fields EditMap

--- src/main/java/com/cs3733/teamd/Controller/AdminScreenController.java
+++
@@ -1,7 +0,0 @@
-package com.cs3733.teamd.Controller;
-
-/**
- * Created by Ryan on 4/6/2017.
- */
-public class AdminScreenController {
-}
--- src/main/java/com/cs3733/teamd/Controller/EditMapScreenController.java
+++ src/main/java/com/cs3733/teamd/Controller/EditMapScreenController.java
@@ -1,7 +1,29 @@
 package com.cs3733.teamd.Controller;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+
+import javax.xml.soap.Text;
+
 /**
  * Created by Anh Dao on 4/6/2017.
  */
 public class EditMapScreenController {
+    public Button EditProf;
+    public Button EditTag;
+    public Slider floorSlider;
+    public Button LoginButton;
+    public Button SpanishButton;
+    public Button addNode;
+    public Button connectNode;
+    public Label xLoc;
+    public Label yLoc;
+    public ImageView floorMap;
+    public TextArea tagList;
+    public TextArea neighborsList;
+
+
 }
