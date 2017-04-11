e07ef61694a9969d7ad0daf0efb57923e840d683
tomkoker <teddy.koker@gmail.com>
2017-04-11 12:45:27 -0400

    started writing more pathfinding tests

--- src/test/java/com/cs3733/teamd/Model/PathfinderTest.java
+++ src/test/java/com/cs3733/teamd/Model/PathfinderTest.java
@@ -30,18 +30,51 @@ public class PathfinderTest {
         Node c = new Node(0, -1,4);
         Node d = new Node(3, -4,4 );
 
+        /*
+
+        a---b
+        |   |
+        c   |
+         \  |
+          \ |
+            d
+
+         */
+
+
         a.addNode(b); a.addNode(c);
-        b.addNode(a); b.addNode(d);
-        c.addNode(d); c.addNode(a);
-        d.addNode(b); d.addNode(c);
+        b.addNode(d);
+        c.addNode(d);
 
         Pathfinder pf = new Pathfinder(a,d);
         LinkedList<Node> calulatedPath = pf.shortestPath();
         LinkedList<Node> actualPath = new LinkedList<>();
         actualPath.add(d); actualPath.add(c); actualPath.add(a); // backwards path
-        for(Node n: calulatedPath){
 
-        }
         assertArrayEquals(calulatedPath.toArray(), actualPath.toArray());
+    }
+
+    @Test
+    public void TestMultiFloor(){
+        Node a = new Node(0, 0, 4);
+        Node b = new Node(0, 0, 3);
+        Node c = new Node(0, 2, 3);
+        Node d = new Node(0, 2, 4);
+
+        Node e = new Node(0, 0, 2);
+        Node f = new Node(0, 4, 3);
+
+        /*
+
+        a   d           floor 4
+        |   |
+        b---c---f       floor 3
+        |   |   |
+        e   g---h       floor 2
+
+        */
+
+
+        Pathfinder pf = new Pathfinder(a,d);
     }
 }
