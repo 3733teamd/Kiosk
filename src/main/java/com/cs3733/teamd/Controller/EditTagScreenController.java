60b78c907f97ffbcf3967e9b2a61248c1a9dad28
Stephen Michelini <sdmichelini@wpi.edu>
2017-04-19 10:25:58 -0400

    Back with Connectables

--- src/main/java/com/cs3733/teamd/Controller/EditTagScreenController.java
+++ src/main/java/com/cs3733/teamd/Controller/EditTagScreenController.java
@@ -3,7 +3,6 @@ package com.cs3733.teamd.Controller;
 import com.cs3733.teamd.Model.Entities.Directory;
 import com.cs3733.teamd.Model.Entities.Professional;
 import com.cs3733.teamd.Model.Entities.Tag;
-import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
@@ -14,26 +13,26 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import org.controlsfx.control.textfield.TextFields;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.stream.Collectors;
 
 /**
  * Created by Anh Dao on 4/6/2017.
  */
 public class EditTagScreenController extends AbsController {
+    public CheckBox selectConnectable;
     Directory dir = Directory.getInstance();
+    @FXML
     public TextArea tagTextArea;
     public TextField searchTagBar;
     public Button BackButton;
     public AnchorPane MMGpane;
+    @FXML
     public Button addNewTag;
     public TextField tagNameTxt;
     @FXML
@@ -46,7 +45,11 @@ public class EditTagScreenController extends AbsController {
     private TextField profSearchField;
 
     List<Professional> filtered = new ArrayList<Professional>();
+    List<Tag> filteredTag = new ArrayList<>();
+
     ObservableList<Professional> searchResults = FXCollections.observableArrayList();
+    ObservableList<Tag> searchResultsTag = FXCollections.observableArrayList();
+
 
     private Tag chosenTag = null;
 
@@ -68,83 +71,21 @@ public class EditTagScreenController extends AbsController {
 
     @FXML
     private ListView<Professional> currentProfessionals;
-    //timeout
-    Timer timer = new Timer();
-    int counter = 0;
-    private volatile boolean running = true;
-
-    TimerTask timerTask = new TimerTask() {
-        @Override
-        public void run() {
-            counter++;
-            System.out.println("editTag counting: "+counter);
-        }
-    };
-
-    Runnable runnable = new Runnable() {
-        @Override
-        public void run() {
-            while (running) {
-                try {
-
-                    if (counter == timeoutTime) {
-                        running = false;
-                        timer.cancel();
-                        timerTask.cancel();
-                        Platform.runLater(resetKiosk);
-                        break;
-                    }
-                    Thread.sleep(1000);
-                } catch (InterruptedException exception) {
-                    timer.cancel();
-                    timerTask.cancel();
-                    running = false;
-                    break;
-                }
-            }
-        }
-    };
-    Thread timerThread = new Thread(runnable);
-    Runnable resetKiosk = new Runnable() {
-        @Override
-        public void run() {
-            timer.cancel();
-            timer.purge();
-            running = false;
-            timerThread.interrupt();
-
-            //logout user
-            dir.logoutUser();
-            try {
-                switchScreen(MMGpane, "/Views/UserScreen.fxml");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-    };
-
 
 
     //Back button
     @FXML
     public void onBack(ActionEvent actionEvent) throws IOException {
-        timer.cancel();
-        timer.purge();
-        running = false;
-        timerThread.interrupt();
         switchScreen(MMGpane, "/Views/EditMapScreen.fxml");
     }
     @FXML
     public void initialize(){
-        timer.scheduleAtFixedRate(timerTask, 30, 1000);
-        timerThread.start();
-
         //make some buttons opaque
         addProf.setOpacity(.5);
         deleteProf.setOpacity(.5);
         newTagNameBtn.setOpacity(.5);
         //disable buttons
+        selectConnectable.setDisable(true);
         addProf.setDisable(true);
         deleteProf.setDisable(true);
         newTagNameBtn.setDisable(true);
@@ -153,7 +94,7 @@ public class EditTagScreenController extends AbsController {
         for (Tag t : dir.getTags()) {
             allTagNames.add(t.getTagName());
         }
-        TextFields.bindAutoCompletion(searchTagBar, allTagNames);
+        /// TextFields.bindAutoCompletion(searchTagBar, allTagNames);//Not being used due to list view
         //List all tags in tagList
         tagList.setItems(FXCollections.observableArrayList(dir.getTags()));
         ObservableList<String> names = FXCollections.observableArrayList((List)dir.getProfessionals());
@@ -189,6 +130,8 @@ public class EditTagScreenController extends AbsController {
                         addProf.setOpacity(1.0);
                         deleteProf.setOpacity(1.0);
                         newTagNameBtn.setOpacity(1.0);
+                        selectConnectable.setSelected(selectedTag.isConnectable());
+                        selectConnectable.setDisable(false);
                         addProf.setDisable(false);
                         deleteProf.setDisable(false);
                         newTagNameBtn.setDisable(false);
@@ -198,22 +141,22 @@ public class EditTagScreenController extends AbsController {
                 });
 
         currentProfessionals.getSelectionModel().selectedItemProperty().addListener(
-        new ChangeListener<Professional>() {
-            public void changed(ObservableValue<? extends Professional> ov,
-                                Professional old_val, Professional new_val) {
-                selectedCurProf = new_val;
+                new ChangeListener<Professional>() {
+                    public void changed(ObservableValue<? extends Professional> ov,
+                                        Professional old_val, Professional new_val) {
+                        selectedCurProf = new_val;
 
-            }
-        });
+                    }
+                });
 
         allProffessionals.getSelectionModel().selectedItemProperty().addListener(
-            new ChangeListener<Professional>() {
-            public void changed(ObservableValue<? extends Professional> ov,
-                                Professional old_val, Professional new_val) {
-                selectedProf = new_val;
+                new ChangeListener<Professional>() {
+                    public void changed(ObservableValue<? extends Professional> ov,
+                                        Professional old_val, Professional new_val) {
+                        selectedProf = new_val;
 
-            }
-        });
+                    }
+                });
 
         profSearchField.setOnKeyPressed(new EventHandler<KeyEvent>() {
             @Override
@@ -223,36 +166,19 @@ public class EditTagScreenController extends AbsController {
                 displayResult(profSearchField.getText() + event.getText());
             }
         });
-        //timer resets if mouse moved
-        MMGpane.setOnMousePressed(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                counter = 0;
-                System.out.println("counter resets");
-            }
-        });
-        MMGpane.setOnMouseMoved(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                counter = 0;
-            }
-        });
-        MMGpane.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                counter = 0;
-            }
-        });
-        MMGpane.setOnKeyPressed(new EventHandler<KeyEvent>() {
+        searchTagBar.setOnKeyPressed(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
-                counter = 0;
+                //String text = searchTagBar.getText();
+
+                displayResultAllTag(searchTagBar.getText() + event.getText());
             }
         });
+
     }
 
     @FXML
-    void newTagName(ActionEvent event) {
+    void modifyTag(ActionEvent event) {
         String noSpace = tagNameTxt.getText().replaceAll("\\s","");
         if (noSpace == null ||noSpace==""|| noSpace.length()<=1) {
             searchTagBar.setText("");
@@ -263,6 +189,12 @@ public class EditTagScreenController extends AbsController {
             tagList.refresh();
             tagNameTxt.clear();
         }
+        if(selectConnectable.isSelected() != selectedTag.isConnectable()) {
+            selectedTag.setConnectable(selectConnectable.isSelected());
+            dir.updateTag(selectedTag);
+            tagList.refresh();
+        }
+
     }
 
     @FXML
@@ -295,6 +227,19 @@ public class EditTagScreenController extends AbsController {
 
         allProffessionals.setItems(searchResults);
     }
+
+    @FXML
+    public void displayResultAllTag(String value){
+
+        for (Tag d: dir.getTags()){
+            filteredTag = dir.getTags().stream().filter((p) -> p.getTagName().toLowerCase().contains(value.toLowerCase())).collect(Collectors.toList());
+        }
+
+        searchResultsTag.setAll(filteredTag);
+
+        tagList.setItems(searchResultsTag);
+    }
+
     //TODO: is deleted from database wrongly, causes fatal error
     @FXML
     void deleteTag(ActionEvent event) {
@@ -305,7 +250,7 @@ public class EditTagScreenController extends AbsController {
             tagList.refresh();
         }
     }
-    
+
     @FXML
     void addTag(ActionEvent event) {
         String noSpace = searchTagBar.getText().replaceAll("\\s","");
