9749039fbdfef00d17f64da054e3d39bfb8d0de7
wittenber0 <ryanwwittenberg@gmail.com>
2017-04-25 13:51:44 -0400

    editMap Color Reformat

--- src/main/java/com/cs3733/teamd/Controller/MapController.java
+++ src/main/java/com/cs3733/teamd/Controller/MapController.java
@@ -200,7 +200,7 @@ public class MapController extends AbsController {
     }
 
     protected void addCircle(Node n, Color c, double r) {
-        CircleNode circle = new CircleNode(getNodeX(n), getNodeY(n), r, c,n);
+        CircleNode circle = new CircleNode(getNodeX(n), getNodeY(n), r,n);
         circle.setCursor(Cursor.HAND);
         circle.setOnMouseEntered((event) -> {
             if(n.getTags().size() > 0) {
@@ -229,12 +229,7 @@ public class MapController extends AbsController {
         circleNodeMap.put(n, circle);
     }
 
-    private CircleNode createDefaultCircle(Node n) {
-        CircleNode circle = new CircleNode(getNodeX(n), getNodeY(n), 5.0, Color.BLUE,n);
-        circle.setCursor(Cursor.HAND);
 
-        return circle;
-    }
 
     protected void drawNodes() {
         mapCanvas.getChildren().clear();
@@ -242,13 +237,13 @@ public class MapController extends AbsController {
         // Draw all of the nodes that are on the current floor
         for(Node n: nodes) {
             CircleNode currentNode = circleNodeMap.get(n);
-            System.out.println(currentNode);
-            if(currentNode == null) {
-                currentNode = createDefaultCircle(n);
-            }
+            System.out.println(currentNode.defaultColor.toString());
+
             // Draw it
             if(n.getFloor() == floor) {
                 mapCanvas.getChildren().removeAll(currentNode);
+                currentNode.setDefaultColor();
+                //System.out.println(currentNode.defaultColor.getRed());
                 mapCanvas.getChildren().add(currentNode);
             }
         }
