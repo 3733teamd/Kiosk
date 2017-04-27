a76b293f0605badb05f8f93f44e8fd639d1835ed
wittenber0 <ryanwwittenberg@gmail.com>
2017-04-27 19:18:51 -0400

    Timer SIngleton

--- src/main/java/com/cs3733/teamd/Model/TimeoutManager.java
+++ src/main/java/com/cs3733/teamd/Model/TimeoutManager.java
@@ -4,4 +4,16 @@ package com.cs3733.teamd.Model;
  * Created by Ryan on 4/27/2017.
  */
 public class TimeoutManager {
+    private static TimeoutManager timer;
+
+    private TimeoutManager(){
+        timer = new TimeoutManager();
+    }
+
+    public TimeoutManager getTimer(){
+        if (timer == null){
+            timer = new TimeoutManager();
+        }
+        return timer;
+    }
 }
