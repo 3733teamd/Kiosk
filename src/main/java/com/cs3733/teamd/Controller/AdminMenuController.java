5602d59dc7ea84d42ff9c6af9f38aa67305ce62b
wittenber0 <ryanwwittenberg@gmail.com>
2017-04-27 14:22:43 -0400

    new AdminMenu

---
+++ src/main/java/com/cs3733/teamd/Controller/AdminMenuController.java
@@ -0,0 +1,35 @@
+package com.cs3733.teamd.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+/**
+ * Created by Ryan on 4/27/2017.
+ */
+public class AdminMenuController extends AbsController{
+
+    public Button toEditMap;
+    public Button toEditTag;
+    public Button toEditProf;
+    public Button toEnhancedMap;
+    public Button toAddUser;
+    public Button toBugView;
+
+    public void goToEditMap(ActionEvent actionEvent) {
+    }
+
+    public void goToEditTag(ActionEvent actionEvent) {
+    }
+
+    public void goToEditProf(ActionEvent actionEvent) {
+    }
+
+    public void goToEnhancedMap(ActionEvent actionEvent) {
+    }
+
+    public void goToAddUser(ActionEvent actionEvent) {
+    }
+
+    public void goToBugView(ActionEvent actionEvent) {
+    }
+}
