2abe2d706755b8a0e483eeaf85dae591670a1a19
wittenber0 <ryanwwittenberg@gmail.com>
2017-04-27 19:24:44 -0400

    Timer SIngleton

--- src/main/java/com/cs3733/teamd/Model/TimeoutManager.java
+++ src/main/java/com/cs3733/teamd/Model/TimeoutManager.java
@@ -1,10 +1,15 @@
 package com.cs3733.teamd.Model;
 
+import com.cs3733.teamd.Controller.AbsController;
+import javafx.scene.layout.Pane;
+
 /**
  * Created by Ryan on 4/27/2017.
  */
 public class TimeoutManager {
     private static TimeoutManager timer;
+    private AbsController currentController;
+
 
     private TimeoutManager(){
         timer = new TimeoutManager();
@@ -16,4 +21,10 @@ public class TimeoutManager {
         }
         return timer;
     }
+
+    public void setCurrentCurrent(AbsController c){
+        currentController = c;
+    }
+
+
 }
